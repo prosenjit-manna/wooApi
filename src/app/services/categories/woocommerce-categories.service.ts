e79ce3c581521236ba0314f61d360f71941b7207
Prosenjit Manna <prosenjit@itobuz.com>
2018-06-08 22:37:22 +0530

    Product categories included

---
+++ src/app/services/categories/woocommerce-categories.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import {
+  ProductCategory,
+  ProductCategoryImage,
+  CategoryQuery
+ } from './product-categories.interface';
+import { WoocommerceHelperService } from '../helper.service';
+
+@Injectable()
+export class WoocommerceProductsService {
+
+  constructor(
+    private httpClient: HttpClient,
+    private wooHelper: WoocommerceHelperService
+  ) { }
+
+  /**
+   * Create a Category
+   * @param payload: Product
+   */
+  createCategory(category: ProductCategory): Observable<ProductCategory> {
+    return this.httpClient.post<ProductCategory>(`categories`, category)
+      .pipe(catchError(err => this.wooHelper.handleError(err)));
+  }
+
+  /**
+   * Retrieve a Category
+   */
+  retrieveCategory(id: number): Observable<ProductCategory> {
+    return this.httpClient.get<ProductCategory>(`categories/${id}`)
+      .pipe(catchError(err => this.wooHelper.handleError(err)));
+  }
+
+  /**
+   * Retrieve list of Category
+   */
+  retrieveCategories(query?: CategoryQuery): Observable<CategoryQuery[]> {
+    return this.httpClient.get(`categories`,
+      {
+        params: this.wooHelper.includeQuery(query)
+      })
+      .pipe(
+        catchError(err => this.wooHelper.handleError(err)));
+  }
+
+  /**
+   * Update Category
+   */
+  updateProduct(id: number, payload: ProductCategory): Observable<ProductCategory> {
+    return this.httpClient.put<ProductCategory>(`categories/${id}`, payload)
+    .pipe(catchError(err => this.wooHelper.handleError(err)));
+  }
+
+  /**
+   * Update Category
+   */
+  deleteProduct(id: number): Observable<ProductCategory> {
+    return this.httpClient.delete<ProductCategory>(`categories/${id}`)
+    .pipe(catchError(err => this.wooHelper.handleError(err)));
+  }
+}
