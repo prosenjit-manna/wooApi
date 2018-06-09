905c632d496c254a023afc20744267e1d74c3984
Prosenjit Manna <prosenjit@itobuz.com>
2018-06-09 21:53:01 +0530

    Api Test code included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AppInterceptor } from './http.interceptor';
 import { WoocommerceProductsService } from '@services/products/woocommerce-products.service';
 import { WoocommerceHelperService } from '@services/helper.service';
+import { AuthService } from '@services/auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { WoocommerceHelperService } from '@services/helper.service';
       multi: true
     },
     WoocommerceProductsService,
-    WoocommerceHelperService
+    WoocommerceHelperService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+// import { ActivatedRoute } from '@angular/router';
+// import { switchMap } from 'rxjs/operators';
 
-import { WoocommerceProductsService } from '@services/products/woocommerce-products.service';
+// import { WoocommerceProductsService } from '@services/products/woocommerce-products.service';
+import { AuthService } from '@services/auth/auth.service';
 
 
 @Component({
@@ -13,24 +14,38 @@ import { WoocommerceProductsService } from '@services/products/woocommerce-produ
 export class HomeComponent implements OnInit {
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private wooProducs: WoocommerceProductsService
+    // private activatedRoute: ActivatedRoute,
+    // private wooProducs: WoocommerceProductsService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
-    this.activatedRoute.queryParams
-      .pipe(
-        switchMap(params => this.wooProducs.retrieveProducts(params))
-      ).subscribe(response => {
-        console.log(response);
-      }, err => {
-        console.log(err);
-      });
+    // this.activatedRoute.queryParams
+    //   .pipe(
+    //     switchMap(params => this.wooProducs.retrieveProducts(params))
+    //   ).subscribe(response => {
+    //     console.log(response);
+    //   }, err => {
+    //     console.log(err);
+    //   });
     // this.wooProducs.retrieveProducts().subscribe(response => {
     //   console.log(response);
     // }, err => {
     //   console.log(err);
     // });
+    this.authService.register({
+      nonce: '9a3d6eb071',
+      username: 'aa',
+      email: 'aa@aa.com',
+      user_pass: 'abc123',
+      notify: 'both',
+      display_name: 'abc'
+    })
+    .subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
