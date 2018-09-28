b6037303dd2c75b886a31488fb98f6cb8d324d1c
Prosenjit Manna <prosenjit@itobuz.com>
2018-09-28 09:31:10 +0530

    duplicate interface removed

--- src/app/services/customer/customer.interface.ts
+++ src/app/services/customer/customer.interface.ts
@@ -18,31 +18,3 @@ export interface Customer {
   avatar_url: string;
   meta_data: Array<any>;
 }
-
-
-export interface Billing {
-  first_name: string;
-  last_name: string;
-  company: string;
-  address_1: string;
-  address_2: string;
-  city: string;
-  state: string;
-  postcode: string;
-  country: string;
-  email: string;
-  phone: string;
-}
-
-export interface Shipping {
-  first_name: string;
-  last_name: string;
-  company: string;
-  address_1: string;
-  address_2: string;
-  city: string;
-  state: string;
-  postcode: string;
-  country: string;
-}
-
