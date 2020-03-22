3e6dada3d8563324a273702c1e83cfef3db8f169
Steven Masala <me@smasala.com>
2020-03-22 16:27:32 +0100

    Update order-notes.service.ts

--- src/app/services/order-notes/order-notes.service.ts
+++ src/app/services/order-notes/order-notes.service.ts
@@ -37,8 +37,8 @@ export class WoocommerceOrderNoteService {
    * List all order notes
    * URL: https://woocommerce.github.io/woocommerce-rest-api-docs/#list-all-order-notes
    */
-  retriveNotes(id: number ): Observable<OrderNoteRes> {
-    return this.httpClient.get<OrderNoteRes>(`orders/${id}/notes/`)
+  retriveNotes(id: number ): Observable<OrderNoteRes[]> {
+    return this.httpClient.get<OrderNoteRes[]>(`orders/${id}/notes/`)
       .pipe(catchError(err => this.wooHelper.handleError(err)));
   }
 
